d11d9cbf1eab35abb13dc34623a56227ec1d6012
Sergey <aseev-1977@list.ru>
2023-02-05 20:06:35 +0300

    task66

--- task66/Program.cs
+++ task66/Program.cs
@@ -3,3 +3,24 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
 
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Введите N:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите M:");
+int n = int.Parse(Console.ReadLine()!);
+Console.ResetColor();
+void CountSum(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        Console.ResetColor();
+        return;
+    }
+    sum = sum + (m++);
+    CountSum(m, n, sum);
+}
+CountSum(m, n, 0);
+
